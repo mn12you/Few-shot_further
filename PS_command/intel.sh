09ba9378d931d5baddd6404e3c3c2eb6db8cc764
s12you <s1212mn@gmail.com>
2024-10-22 18:03:01 +0800

    modified:   PS_command/intel.sh

--- PS_command/intel.sh
+++ PS_command/intel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -A ACD112176      # Account name/project number
-#SBATCH -J Corr_index generate    # Job name
-#SBATCH -p ct560             # Partition name
-#SBATCH -c 10              # Number of cores per MPI task
-#SBATCH -n 48               # Number of MPI tasks (i.e. processes)
+#SBATCH -J Corr_index    # Job name
+#SBATCH -p ct224             # Partition name
+#SBATCH --ntasks-per-node 28
+#SBATCH -N 5              # Number of MPI tasks (i.e. processes)
 #SBATCH -o TWCC_log/%j.out           # Path to the standard output file
 #SBATCH -e TWCC_log/%j.err           # Path to the standard error ouput file
 #SBATCH --mail-user=s1212mn@gmail.com   # email
